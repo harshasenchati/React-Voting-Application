5079fe59fceb34a8b76fb366eb1d6bdc2bc4bc2c
Harsha Senchati <senchatiharsha5@gmail.com>
2024-08-04 23:32:17 +0530

    rectified errors

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,11 @@
-
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function Results({ teams }) {
   return (
-    <div className='results'>
-      <h2></h2>
-      {teams.map(team => (
-        <div key={team.id} className='items' >
+    <div className="results">
+      {teams.map((team) => (
+        <div key={team.id} className="items">
           <h3>{team.name}</h3>
           <p>Votes: {team.votes}</p>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
